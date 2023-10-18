4861bb52e483716077d3d532b42cc44708634728
Mr-KulwinderSingh Sidhu <k29singh@gmail.com>
2023-10-18 23:05:12 +0000

    Create the super user, add summernote editor for the admin to write a post  set it up in the settings and urls file

--- iphoneblog/models.py
+++ iphoneblog/models.py
@@ -4,10 +4,12 @@ from cloudinary.models import CloudinaryField
 
 STATUS = ((0, "Draft"), (1, "Published"))
 
+
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="blog_post")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="blog_post")
     updated_on = models.DateTimeField(auto_now=True)
     content = models.TextField()
     featured_image = CloudinaryField('image', default='placeholder')
